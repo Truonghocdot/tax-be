4ab345b5b6ed11cbb4bc3d543a27b24fb3569d17
Truonghocdot <truonghocdot05@gmail.com>
2026-02-03 19:03:17 +0700

    Truonghd - Fix

--- app/Http/Resources/QrBankConnectResource.php
+++ app/Http/Resources/QrBankConnectResource.php
@@ -20,7 +20,7 @@ class QrBankConnectResource extends JsonResource
             'bin_bank' => $this->bin_bank,
             'number_account' => $this->number_account,
             'amount' => $this->amount,
-            'description' => $this->description,
+            'description' => $this->description ?? '',
         ];
     }
 }
